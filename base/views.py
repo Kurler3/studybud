73b94338930d9d2dd4007591024816380d2338a7
Kurler3 <miguelbento2000@gmail.com>
2022-08-31 08:40:09 +0100

    Hide the secret key

--- base/views.py
+++ base/views.py
@@ -35,3 +35,7 @@ def room(request, pk):
     context = {'room': room}
     
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    
+    return render(request, 'base/room_form.html')
